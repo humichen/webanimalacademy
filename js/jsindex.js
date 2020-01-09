9d976d905d2e31f548ac232cf2c5250436a29d57
humichen <43415952+humichen@users.noreply.github.com>
2020-01-09 09:55:24 +0800

    Add files via upload

--- js/jsindex.js
+++ js/jsindex.js
@@ -1,10 +1,16 @@
 $(document).ready(function () {
-        $(window).scroll(function () {
+    if (window.screen.width>768 && window.screen.width>=window.screen.height) {
+         $(window).scroll(function () {
         if ($(this).scrollTop() > 300) {
             $('.column-header').css('opacity', '1').css('transform','translate(0,0)').css('transition', '.50s ease');
             $('.container-list-layout').css('opacity', '1').css('transform','translate(0,0)').css('transition', '.90s ease');
         }
     }).scroll();
+    }
+    else{
+        setTimeout(function () { $('.column-header').css('opacity', '1').css('transform','translate(0,0)').css('transition', '.50s ease');$('.container-list-layout').css('opacity', '1').css('transform','translate(0,0)').css('transition', '.90s ease'); }, 300);
+    }    
+   
     
 
     
