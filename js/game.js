84ff04c7b4488ac370950921ddf2cd573cf04cf1
humichen <43415952+humichen@users.noreply.github.com>
2020-01-09 10:02:45 +0800

    Add files via upload

--- js/game.js
+++ js/game.js
@@ -20,7 +20,8 @@ $(document).ready(function () {
         setTimeout(function () { $(".move3").css('transform', 'translate(0,5%)').css('transition', '.2s ease') }, 900);
         setTimeout(function () { $(".move3").css('transform', 'translate(0,0)').css('transition', '.2s ease') }, 1100);
     });
-    $(window).scroll(function () {
+    if((window.screen.width>768 && window.screen.width>=window.screen.height) || window.screen.width<=425){
+       $(window).scroll(function () {
         if ($(this).scrollTop() >= 10) {
             setTimeout(function () { $(".img1").css('opacity', '1').css('transform', 'translate(0,0)').css('transition', '.5s ease') }, 300);
             setTimeout(function () { $(".img2").css('opacity', '1').css('transform', 'translate(0,0)').css('transition', '.5s ease') }, 600);
@@ -32,6 +33,19 @@ $(document).ready(function () {
             setTimeout(function () { $(".img8").css('opacity', '1').css('transform', 'translate(0,0)').css('transition', '.5s ease') }, 2400);
             setTimeout(function () { $(".img9").css('opacity', '1').css('transform', 'translate(0,0)').css('transition', '.5s ease') }, 2700);
         }
-    }).scroll();
+    }).scroll(); 
+    }
+    else{
+        setTimeout(function () { $(".img1").css('opacity', '1').css('transform', 'translate(0,0)').css('transition', '.5s ease') }, 300);
+            setTimeout(function () { $(".img2").css('opacity', '1').css('transform', 'translate(0,0)').css('transition', '.5s ease') }, 600);
+            setTimeout(function () { $(".img3").css('opacity', '1').css('transform', 'translate(0,0)').css('transition', '.5s ease') }, 900);
+            setTimeout(function () { $(".img4").css('opacity', '1').css('transform', 'translate(0,0)').css('transition', '.5s ease') }, 1200);
+            setTimeout(function () { $(".img5").css('opacity', '1').css('transform', 'translate(0,0)').css('transition', '.5s ease') }, 1500);
+            setTimeout(function () { $(".img6").css('opacity', '1').css('transform', 'translate(0,0)').css('transition', '.5s ease') }, 1800);
+            setTimeout(function () { $(".img7").css('opacity', '1').css('transform', 'translate(0,0)').css('transition', '.5s ease') }, 2100);
+            setTimeout(function () { $(".img8").css('opacity', '1').css('transform', 'translate(0,0)').css('transition', '.5s ease') }, 2400);
+            setTimeout(function () { $(".img9").css('opacity', '1').css('transform', 'translate(0,0)').css('transition', '.5s ease') }, 2700);
+    }
+    
         
 });
